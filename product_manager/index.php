1ce2a5a9f1e2914ceb5f6818ad00a1ecc23a905b
ddo6 <ddo6@njit.edu>
2017-04-26 18:44:36 -0400

    Update index.php

--- product_manager/index.php
+++ product_manager/index.php
@@ -30,5 +30,14 @@ if($action == 'list_products'){
     $version = $_POST['version'];
     $release_date = $_POST['release_date'];
     }
-    
+
+    //Input Validation
+   if (empty($code) || emty($name) ||  
+   empty($version) || empty($release_date)) {
+        $error = "Invalid product data. Check all fields and try again.";
+        include('../errors/error.php');
+    } else {
+        add_product($code, $name, $version, $release_date);
+        header("Location: .");
+    } 
 ?>
