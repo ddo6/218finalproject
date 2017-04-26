b0797febcf316c7b590c059ccacad8f524f73385
ddo6 <ddo6@njit.edu>
2017-04-26 18:48:23 -0400

    Update index.php

--- product_manager/index.php
+++ product_manager/index.php
@@ -1,43 +1,45 @@
 <?php
+session_start();
+
+// Create a session array if needed
+if (empty($_SESSION['tech_support'])) $_SESSION['tech_support'] = array();
+
 require('../model/database.php');
 require('../model/product_db.php');
 
-$action = filter_input(INPUT_POST, 'action');
-if ($action == NULL) {
-    $action = filter_input(INPUT_GET, 'action');
-    if ($action == NULL) {
-        $action = 'list_products';
-    }
+if (isset($_POST['action'])) {
+    $action = $_POST['action'];
+} else if (isset($_GET['action'])) {
+    $action = $_GET['action'];
+} else {
+    $action = 'list_products';
 }
 
-if($action == 'list_products'){
-    
+if ($action == 'list_products') {
+    // Get product data
     $products = get_products();
-    
+
+    // Display the product list
     include('product_list.php');
-    } 
-    else if ($action == 'delete_product') {
+} else if ($action == 'delete_product') {
     $product_code = $_POST['product_code'];
     delete_product($product_code);
     header("Location: .");
-    } 
-    else if ($action == 'show_add_form') {
+} else if ($action == 'show_add_form') {
     include('product_add.php');
-    } 
-    else if ($action == 'add_product') {
+} else if ($action == 'add_product') {
     $code = $_POST['code'];
     $name = $_POST['name'];
     $version = $_POST['version'];
     $release_date = $_POST['release_date'];
-    }
 
-    //Input Validation
-   if (empty($code) || emty($name) ||  
-   empty($version) || empty($release_date)) {
+    // Validate the inputs
+    if (empty($code) || empty($name) || empty($version) || empty($release_date)) {
         $error = "Invalid product data. Check all fields and try again.";
         include('../errors/error.php');
     } else {
         add_product($code, $name, $version, $release_date);
         header("Location: .");
-    } 
+    }
+}
 ?>
