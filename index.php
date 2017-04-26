ae306765aed6cb004ba9be3b876af9d1b6964c63
ddo6 <ddo6@njit.edu>
2017-04-26 19:09:11 -0400

    Update index.php

--- index.php
+++ index.php
@@ -4,12 +4,12 @@
         
     <h2>Administrators</h2>
     <ul>
-        <li><a href="/product_manager">Manage Products</a></li>
-        <li><a href="/technician_manager">Manage Technicians</a></li>
-        <li><a href="/under_construction.php">Manage Customers</a></li>
-        <li><a href="/under_construction.php">Create Incident</a></li>
-        <li><a href="/under_construction.php">Assign Incident</a></li>
-        <li><a href="/under_construction.php">Display Incidents</a></li>
+        <li><a href="product_manager/">Manage Products</a></li>
+        <li><a href="technician_manager/">Manage Technicians</a></li>
+        <li><a href="under_construction.php">Manage Customers</a></li>
+        <li><a href="under_construction.php">Create Incident</a></li>
+        <li><a href="under_construction.php">Assign Incident</a></li>
+        <li><a href="under_construction.php">Display Incidents</a></li>
     </ul>
 
     <h2>Technicians</h2>    
