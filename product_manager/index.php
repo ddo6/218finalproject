87d8c44e7ca3a67603af7b7220af5908c2c078c8
ddo6 <ddo6@njit.edu>
2017-04-26 18:20:58 -0400

    Update index.php

--- product_manager/index.php
+++ product_manager/index.php
@@ -10,65 +10,47 @@ if ($action == NULL) {
         $action = 'list_products';
     }
 }
-
-if ($action == 'list_products') {
-    $category_id = filter_input(INPUT_GET, 'category_id', 
-            FILTER_VALIDATE_INT);
-    if ($category_id == NULL || $category_id == FALSE) {
-        $category_id = 1;
-    }
-    $category_name = get_category_name($category_id);
-    $categories = get_categories();
-    $products = get_products_by_category($category_id);
-    include('product_list.php');
-} else if ($action == 'delete_product') {
-    $product_id = filter_input(INPUT_POST, 'product_id', 
-            FILTER_VALIDATE_INT);
-    $category_id = filter_input(INPUT_POST, 'category_id', 
-            FILTER_VALIDATE_INT);
-    if ($category_id == NULL || $category_id == FALSE ||
-            $product_id == NULL || $product_id == FALSE) {
-        $error = "Missing or incorrect product id or category id.";
-        include('../errors/error.php');
-    } else { 
-        delete_product($product_id);
-        header("Location: .?category_id=$category_id");
-    }
-} else if ($action == 'show_add_form') {
-    $categories = get_categories();
-    include('product_add.php');    
-} else if ($action == 'add_product') {
-    $category_id = filter_input(INPUT_POST, 'category_id', 
-            FILTER_VALIDATE_INT);
-    $code = filter_input(INPUT_POST, 'code');
-    $name = filter_input(INPUT_POST, 'name');
-    $price = filter_input(INPUT_POST, 'price');
-    if ($category_id == NULL || $category_id == FALSE || $code == NULL || 
-            $name == NULL || $price == NULL || $price == FALSE) {
-        $error = "Invalid product data. Check all fields and try again.";
-        include('../errors/error.php');
-    } else { 
-        add_product($category_id, $code, $name, $price);
-        header("Location: .?category_id=$category_id");
-    }
-} else if ($action == 'list_categories') {
-    $categories = get_categories();
-    include('category_list.php');
-} else if ($action == 'add_category') {
-    $name = filter_input(INPUT_POST, 'name');
-
-    // Validate inputs
-    if ($name == NULL) {
-        $error = "Invalid category name. Check name and try again.";
-        include('view/error.php');
-    } else {
-        add_category($name);
-        header('Location: .?action=list_categories');  // display the Category List page
-    }
-} else if ($action == 'delete_category') {
-    $category_id = filter_input(INPUT_POST, 'category_id', 
-            FILTER_VALIDATE_INT);
-    delete_category($category_id);
-    header('Location: .?action=list_categories');      // display the Category List page
+// products
+switch($action) {
+    case 'list_products':
+        // list all products
+        $products = get_all_products();
+        include('product_list.php');
+        break;
+    case $action == 'delete_product':
+        // delete a product
+        $code = filter_input(INPUT_POST, 'code');
+        if ($code == NULL || $code == FALSE) {
+            $error = "Missing or incorrect product code.";
+            include('../../errors/error.php');
+        } else { 
+            delete_product($code);
+            header("Location: .");
+        }
+        break;
+    case 'show_add_form':
+        // call page to add a product
+        include('product_add.php'); 
+        break;
+    case 'add_product':
+        // add a product
+        $code = filter_input(INPUT_POST, 'code');
+        $name = filter_input(INPUT_POST, 'name');
+        $version = filter_input(INPUT_POST, 'version');
+        $date = filter_input(INPUT_POST, 'date');
+        if ($code == NULL || $code == FALSE || 
+            $name == NULL || $name == FALSE || 
+            $version == NULL || $version == FALSE ||
+            $date == NULL || $date == FALSE) {
+            $error = "Invalid product data. Check all fields and try again.";
+            include('../../errors/error.php');
+        } else { 
+            add_product($code, $name, $version, $date);
+            header("Location: .");
+        }
+        break;
+    default:
+        display_error("Unknown action: " . $action);
+        break;
 }
 ?>
