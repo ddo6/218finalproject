213a13c4e08e25d2bee0d08838e9e0976c42374a
ddo6 <ddo6@njit.edu>
2017-04-26 18:22:48 -0400

    Update index.php

--- product_manager/index.php
+++ product_manager/index.php
@@ -1,7 +1,5 @@
 <?php
-require('../../model/database.php');
-require('../../model/product_db.php');
-require('../../model/category_db.php');
+require('../model/database.php');
 
 $action = filter_input(INPUT_POST, 'action');
 if ($action == NULL) {
